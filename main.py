f1b944793ec8f6401defd21db88492eb1c10168c
Brook <salamajonga@gmail.com>
2024-09-14 20:10:41 -0300

    some changes for better organization

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from xvideos_api.xvideos_api import Client as clientxvideos
 from firebase_connection import FirebaseConnection
 from wordpress_controller import WordpressAPI
 from video_processing import VideoSearcher, SearchConfig
+import datetime
 
 inColab = "google.colab" in sys.modules
 userdata= None
@@ -19,7 +20,10 @@ prefixMode = "DEBUG" if mode == "debug" else "PROD"
 wpuser = userdata.get(f'WP_USER_{prefixMode}') if userdata else os.getenv(f'WP_USER_{prefixMode}')
 wppass = userdata.get(f'WP_PASS_{prefixMode}') if userdata else os.getenv(f'WP_PASS_{prefixMode}')
 wpurl = userdata.get(f'WP_URL_{prefixMode}') if userdata else os.getenv(f'WP_URL_{prefixMode}')
-print(f'{wpuser}:{wppass}--{wpurl}')
+current_time = datetime.datetime.now()
+
+
+print(f"-----------------------------------------------\nData: {current_time.strftime("%d/%m/%Y %H:%M:%S")}\n", )
 
 wpAPI = WordpressAPI(wpurl,wpuser,wppass)
 firebase_connection = FirebaseConnection('servicekey.json', 'https://alimentsite-86639-default-rtdb.firebaseio.com/', 'connsite')
@@ -50,3 +54,5 @@ clientes = {
 
 video_searcher = VideoSearcher(clientes, default_config,wpAPI,firebase_connection)
 video_searcher.search_and_add_videos()
+
+print(f"-----------------------------------------------\n", )
